c05416c673b64aced128e6a9b5f576922501f46d
andreas-hadimulyono <andreas.hadimulyono@gmail.com>
2015-07-11 19:23:40 +0800

    plot4
    Also changed cex in legend instead of in par

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-generate_plot_3 <- function(){
+generate_plot_4 <- function(){
     ## read the text file
     original_data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
     
@@ -8,21 +8,37 @@ generate_plot_3 <- function(){
     filtered_data <- original_data[original_data$Date == as.Date("2007-02-01") | original_data$Date == as.Date("2007-02-02"),]
     
     ## converting the required column to numeric
+    filtered_data$Global_active_power <- as.numeric(as.character(filtered_data$Global_active_power))
     filtered_data$Sub_metering_1 <- as.numeric(as.character(filtered_data$Sub_metering_1))
     filtered_data$Sub_metering_2 <- as.numeric(as.character(filtered_data$Sub_metering_2))
+    filtered_data$Voltage <- as.numeric(as.character(filtered_data$Voltage))
+    filtered_data$Global_reactive_power <- as.numeric(as.character(filtered_data$Global_reactive_power))
     
-    ## plot
+    par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
+    
+    ## plot 1, top left
+    plot(filtered_data$Time,
+         filtered_data$Global_active_power, 
+         type="l", 
+         ylab="Global Active Power", 
+         xlab="")
+    
+    ## plot 2, top right
+    plot(filtered_data$Time, 
+         filtered_data$Voltage,type="l", 
+         ylab="Voltage", 
+         xlab="datetime")
+    
+    ## plot 3, bottom left
     plot(filtered_data$Time,
          filtered_data$Sub_metering_1, 
          type="l", 
          xlab = "", 
          ylab="Energy sub metering")
-    
     lines(filtered_data$Time,
           filtered_data$Sub_metering_2, 
           type="l", 
           col="red")
-    
     lines(filtered_data$Time,
           filtered_data$Sub_metering_3, 
           type="l", 
@@ -32,8 +48,16 @@ generate_plot_3 <- function(){
            lwd=1, 
            col=c("black", "red", "blue"), 
            legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-           pt.cex = 0.5)
+           pt.cex = 0.5,
+           cex = 0.5,
+           bty="n")
     
-    dev.copy(png, "plot3.png")
+    ## plot 4, bottom right
+    plot(filtered_data$Time, 
+         filtered_data$Global_reactive_power,type="l", 
+         ylab="Global_reactive_power", 
+         xlab="datetime")
+    
+    dev.copy(png, "plot4.png")
     dev.off()
 }
